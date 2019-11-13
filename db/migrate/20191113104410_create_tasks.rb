64e5f516a97502ad10feb01ae387dc6c5dd5ec1f
Maria M <mariamoll@gmx.de>
2019-11-13 11:44:26 +0100

    model

--- db/migrate/20191113104410_create_tasks.rb
+++ db/migrate/20191113104410_create_tasks.rb
@@ -2,8 +2,10 @@ class CreateTasks < ActiveRecord::Migration[5.2]
   def change
     create_table :tasks do |t|
       t.string :title
-      t.text :detail
-      t.boolean :completed, default: false
+      t.text :details
+      t.completed, :
+      t.string :default
+      t.string :false
 
       t.timestamps
     end
