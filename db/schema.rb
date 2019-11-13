07b8d22c17423d22b386c46dc14b8f8167bfa35e
Maria M <mariamoll@gmx.de>
2019-11-13 12:32:10 +0100

    testing crap

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_13_110244) do
+ActiveRecord::Schema.define(version: 2019_11_13_111641) do
 
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 2019_11_13_110244) do
 
   create_table "reviews", force: :cascade do |t|
     t.text "content"
+    t.integer "rating"
     t.integer "restaurant_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -30,5 +31,3 @@ ActiveRecord::Schema.define(version: 2019_11_13_110244) do
   end
 
 end
-
-
