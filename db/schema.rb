923337b54d2a68bd45e6b2e0f90d4093883341cb
Maria M <mariamoll@gmx.de>
2019-11-13 12:27:40 +0100

    trash

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_12_133105) do
+ActiveRecord::Schema.define(version: 2019_11_13_110244) do
 
-  create_table "tasks", force: :cascade do |t|
-    t.string "title"
-    t.text "detail"
-    t.boolean "completed", default: false
+  create_table "restaurants", force: :cascade do |t|
+    t.string "name"
+    t.string "address"
+    t.string "phone"
+    t.string "category"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
+  create_table "reviews", force: :cascade do |t|
+    t.text "content"
+    t.integer "restaurant_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["restaurant_id"], name: "index_reviews_on_restaurant_id"
+  end
+
 end
+
+
--- test/models/review_test.rb
+++ test/models/review_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class TaskTest < ActiveSupport::TestCase
+class ReviewTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
