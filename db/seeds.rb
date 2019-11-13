723368267041495bce9e572badcd072fc3f3079d
Maria M <mariamoll@gmx.de>
2019-11-13 11:35:12 +0100

    rails new

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Task.create(title: 'Laundry', detail: 'I need to wash my towels')
+Task.create(title: 'Dishes', detail: 'I need to wash my dishes')
+Task.create(title: 'Pay Bills', detail: 'I hate everything')
